763d5755536a51c89b4f5c9069efd5756ce2ab3a
Malik950 <54435682+Malik950@users.noreply.github.com>
2020-10-21 15:30:40 +0200

    Add files via upload

---
+++ Playerbase.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Template
+{
+    class Playerbase 
+    {
+        Texture2D texture;
+        protected Vector2 xwingpos = new Vector2(100,100);
+
+        public virtual void Update()
+        {
+
+        }
+
+        public Playerbase(Texture2D tex)
+        {
+            texture = tex;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, xwingpos, Color.White);
+        }
+
+    }
+}
---
+++ player.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template
+{
+    class Player : Playerbase
+    {
+        public Player(Texture2D tex) : base(tex)
+        {
+
+        }
+        public override void Update()
+        {
+            KeyboardState kstate = Keyboard.GetState();
+            if (kstate.IsKeyDown(Keys.Right))
+                xwingpos.X++;
+            if (kstate.IsKeyDown(Keys.Left))
+                xwingpos.X--;
+            if (kstate.IsKeyDown(Keys.Up))
+                xwingpos.Y--;
+            if (kstate.IsKeyDown(Keys.Down))
+                xwingpos.Y++;
+
+
+
+        }
+
+    }
+}
+
+    
+    
+
+
